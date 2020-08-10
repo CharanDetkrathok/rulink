770d9d5faa924a6106c99b4074d34981fd74baf5
CharanDetkrathok <charandetkrathok@gmail.com>
2020-08-10 11:25:20 +0700

    URL http://sevkn.ru.ac.th/rulink Done 99.999999% 10-08-2563

--- src/java/com/rulink/model/DatabaseInfo.java
+++ src/java/com/rulink/model/DatabaseInfo.java
@@ -2,9 +2,10 @@ package com.rulink.model;
 
 public class DatabaseInfo {
     
+//    public static final String URL = "jdbc:mysql://127.0.0.1:3306/rulink?zeroDateTimeBehavior=convertToNull&autoReconnect=true&characterEncoding=UTF-8&characterSetResults=UTF-8";
     public static final String URL = "jdbc:mysql://127.0.0.1:3306/rulink?zeroDateTimeBehavior=convertToNull&autoReconnect=true&characterEncoding=UTF-8&characterSetResults=UTF-8";
     public static final String  USER = "root";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "aabb1122";
     public static final String DRIVER = "com.mysql.jdbc.Driver";
 
 //    public static final String URL = "jdbc:oracle:thin:@10.2.1.98:1521:RUBRAM";
